b7cb46159dde7ef3bbec2e55de87364321e01439
Lucky <spiderkiller6@gmail.com>
2020-05-20 11:29:05 +0800

    Finish non-VPN dev

--- spider.py
+++ spider.py
@@ -1,121 +1,142 @@
-'''
-This is a spider sciprt that can get data from cross_website
-Output : cross_data.csv
-'''
-
-import bs4
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
 from selenium import webdriver
-if __name__ == '__main__':
-    def parse_table(table):
-        '''
-        Output : 
-            (  [('國立台灣大學 機械工程學系','正取'), ('昆蟲系','備取'), .... ]  ,  ('國立台灣大學 機械工程學系','正取') )
-        '''
-        priority_list = [] 
-        medal_ans  = None 
-        rows = table.find('div').find_all('tr')
-        for row in rows:
-            medal, school, result = row.findChildren('td', recursive=False) # Get infomation from every column
-            # isMedal = medal.find('div') is None
-            #----- Get school -------# 
-            try:
-                school = school.find('div').find('a').contents[0] # .strip()
-                school = school.replace('\n',' ') # Delete '\n' in the middle
-            except IndexError:
-                continue
-            #----- Get result -----# 
-            result = result.find_all('div')[0]
-            try: 
-                result  = result.contents[0].find_all('div')[0].string # Not first line 
-            except AttributeError: # First line 
-                result  = result.find_all('div')[0].string
-            #---- Get medal --------#
-            if medal_ans == None:
-                medal = medal.find("img")
-                if medal != None :
-                    medal_ans = (school,result)
-            # ---- append a priority to output  -------# 
-            priority_list.append((school,result))
-        return (priority_list,medal_ans)
-    
-    def save_student_list_unstructure(student_list_unstructure):
-        '''
-        Save student_list_unstructure to python file 
-        '''
-        file = open('student_list_unstructure.py','w')
-        file.write("student_list_unstructure = " + student_list_unstructure.__str__())
-        file.close()
+import datetime
+import time 
+import random 
+#--- parameters ----# 
+ACOUNT   = "yujia0827"
+PASSWORD = "doctorso3_A"
+ENTRY_WEBSIDE = "https://my.ntu.edu.tw/attend/ssi.aspx"
+CHECKIN_HOUR = "11" # Which hour to checkin
+CHECKOUT_HOUR = "18" # Which hour to checkout 
+CHECKIO_MINUTE = (15,45) # Which minute area to do io
+SLEEP_INTERVAL = 30 # sec 
 
-    def parse2(response):
-        print ('you are here!')
-    #---- Get uri content -------# 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-extensions')
-    options.add_argument('--headless') # Run chrome without GUI
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox') # Run chrome with root
-    # options.add_argument("--disable-dev-shm-usage") # For err msg 
-    # chrome_options.add_argument('blink-settings=imagesEnabled=false') # Dont load picture 
-    # browser = webdriver.Chrome('/usr/local/share/chromedriver',chrome_options=options)
-    browser = webdriver.Chrome(chrome_options=options)
+# --- global variable ----# 
+today_check_in_time =["date","hour", "minute" , "sec"]
+today_check_out_time=["date","hour", "minute" , "sec"]
 
-    # ------ Get URI for every department ----------# 
-    browser.get("https://my.ntu.edu.tw/attend/ssi.aspx")
-    # html_source = browser.page_source
-    # soup = BeautifulSoup(html_source, 'html.parser')
-    ans = browser.find_element_by_link_text('登入')
-    Request('https://web2.cc.ntu.edu.tw/p/s/login2/p1.php',callback=parse2)
-    ans.click()
-    '''
-    uri_list = []
-    for tr in soup.find_all("tbody")[2].findChildren('tr', recursive=False): # Start from 2
-        td = tr.findChildren('td', resursive=False)
-        if len(td) != 5 :  # Not a legal row 
-            continue
-        uri_tail = td[1].find("a")['href']
-        uri_list.append("https://www.com.tw/cross/" + uri_tail)
-    '''
-    '''
-    # student_list_unstructure = []
-    std_id_list = []
-    std_name_list = []
-    std_regist_list = []
-    std_priority_list = [[], [] ,[] ,[] ,[] ,[] ,[] ,[] ,[] ,[]]
+class Spider():
+    def __init__(self):
+        #---- Get uri content -------# 
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--disable-extensions')
+        # options.add_argument('--headless') # Run chrome without GUI
+        self.options.add_argument('--disable-gpu')
+        self.options.add_argument('--no-sandbox') # Run chrome with root
+        # options.add_argument("--disable-dev-shm-usage") # For err msg 
+        # chrome_options.add_argument('blink-settings=imagesEnabled=false') # Dont load picture 
+        # browser = webdriver.Chrome('/usr/local/share/chromedriver',chrome_options=options)
+
+    def auto_check_in(self):
+        browser = webdriver.Chrome(chrome_options=self.options)
+        browser.get(ENTRY_WEBSIDE)
     
-    for uri in uri_list: 
-        # ------- For every department ---------# 
-        browser.get(uri)
-        soup_dep = BeautifulSoup(browser.page_source, 'html.parser')
-        stud_data = soup_dep.find_all("tbody")[2].findChildren('tr', recursive=False)
+        browser.find_element_by_link_text('登入').click()
         
-        for tr in stud_data[3:]:
-            target = tr.findChildren('td', recursive=False)
-            if len(target) != 5 : # Not a legal data format 
-                continue # Igorn this row 
-            id, name, table = target[2:]
-            #---- Get ID ------# 
-            std_id = id.find('div').contents[0].strip()
-            if std_id in std_id_list:
-                continue
-            table_result = parse_table(table)
-            #             ID    ,            name           , score ,   priority list ,    regist
-            # student = ( std_id  ,  name.contents[0].strip() ,   []    , table_result[0] , table_result[1])
-            # student_list_unstructure.append(student)
-            #------- Pandas's Dataframe -------#
-            std_id_list.append(std_id)
-            std_name_list.append(name.contents[0].strip())
-            try: 
-                std_regist_list.append(table_result[1][0]) # Only department and school is recorded.
-            except TypeError: # table_result[1] ==  None
-                std_regist_list.append(None)
+        #---- Myntu login ------# 
+        user_acount = browser.find_element_by_name('user')
+        user_acount.click()
+        user_acount.clear()
+        user_acount.send_keys(ACOUNT) # type account name 
 
-            for i in range(len(std_priority_list)):
-                try: 
-                    std_priority_list[i].append(table_result[0][i][0] + " " + str(table_result[0][i][1])) # [('國立台灣大學 機械工程學系','正取'), ('昆蟲系','備取'), .... ] 
-                except IndexError: 
-                    std_priority_list[i].append(None)
-        '''
+        # ---- type password------#
+        password = browser.find_element_by_name('pass')
+        password.click()
+        password.clear()
+        password.send_keys(PASSWORD)  
+
+        #--- click login button -----# 
+        login = browser.find_element_by_name('Submit')
+        login.click()
+        
+        # ----- click check in button -----# 
+        check_in = browser.find_element_by_id('btSign')
+        check_in.click()
+        
+        #--- close browser ---# 
+        time.sleep(3)
+        browser.close()
+    
+    def auto_check_out(self):
+        browser = webdriver.Chrome(chrome_options=self.options)
+        browser.get(ENTRY_WEBSIDE)
+    
+        browser.find_element_by_link_text('登入').click()
+        
+        #---- Myntu login ------# 
+        user_acount = browser.find_element_by_name('user')
+        user_acount.click()
+        user_acount.clear()
+        user_acount.send_keys(ACOUNT) # type account name 
+
+        # ---- type password------#
+        password = browser.find_element_by_name('pass')
+        password.click()
+        password.clear()
+        password.send_keys(PASSWORD)  
+
+        #--- click login button -----# 
+        login = browser.find_element_by_name('Submit')
+        login.click()
+        
+        # ----- click check in button -----# 
+        check_out = browser.find_element_by_id('btSign2')
+        check_out.click()
+
+        #--- close browser ---# 
+        time.sleep(3)
+        browser.close()
+
+
+
+def cal_check_IO_time ():
+    '''
+    Plan when to checkin and checkout today 
+    This function should only be execute once per day 
+    '''
+    global today_check_in_time, today_check_out_time 
+    today_check_in_time[0] = datetime.datetime.now().__str__().split()[0] # '2020-05-20'
+    today_check_in_time[1] = CHECKIN_HOUR
+    today_check_in_time[2] = random.randint(CHECKIO_MINUTE[0],CHECKIO_MINUTE[1])
+    today_check_in_time[3] = random.randint(0,60)
+    print ("Plan to check in at : " + str(today_check_in_time))
+    
+    today_check_out_time[0] = datetime.datetime.now().__str__().split()[0] # '2020-05-20'
+    today_check_out_time[1] = CHECKOUT_HOUR
+    today_check_out_time[2] = random.randint(CHECKIO_MINUTE[0],CHECKIO_MINUTE[1])
+    today_check_out_time[3] = random.randint(0,60)
+    print ("Plan to check out at : " + str(today_check_out_time))
+
+def main ():
+    # global today_check_in_time, today_check_out_time
+    last_checkin_date  = "2020-05-19"
+    last_checkout_date = "2020-05-19"
+    spider = Spider()
+    while True:
+        T = datetime.datetime.now().__str__().split() #['2020-05-20', '10:14:32.086912']
+
+        # ----- Do we plan today checkI/O time already ? ------#  
+        if today_check_in_time[0] == T[0]: # Yes, plan before
+            print ("Today plan before")
+            pass 
+        else: # No, plan one now
+            cal_check_IO_time()
+
+        (hour,minute,sec) = T[1].split(':') # Current time
+        # check if now is the today today_check_in_time  
+        # if last_checkin_date != today_check_in_time[0] and hour == today_check_in_time[1] and minute == today_check_in_time[2]: # TODO 
+        if last_checkin_date != today_check_in_time[0]:
+            print ("Check In !!!" + str(T))
+            spider.auto_check_in()
+            last_checkin_date = today_check_in_time[0]
+        # elif last_checkout_date != today_check_out_time[0] and hour == today_check_out_time[1] and minute == today_check_out_time[2]: # TODO 
+        elif last_checkout_date != today_check_out_time[0]:
+            print ("Check out!"  + str(T))
+            spider.auto_check_out()
+            last_checkout_date = today_check_out_time[0]
+        time.sleep(SLEEP_INTERVAL)
+
+if __name__ == '__main__':
+    main()
+
 
